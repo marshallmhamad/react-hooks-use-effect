e8ad486568bc913ab3cef6c26a224f0d3bf03ecc
marshall <mhamad.marshall@gmail.com>
2022-12-02 15:25:37 +0300

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
 
 function App() {
+  const [click,setClick] = useState(false)
   useEffect(() => {
     console.log("useEffect called");
   });
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <button onClick={()=> setClick(!click)}>Click Me</button>
+  <DogPics click={click}/>
+    </div>
+
+  )
 }
 
 export default App;
